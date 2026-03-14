2647045224d3a5e69dcd15f48ada7d28cfb71711
David Y.C. Zhou <yaochengzhou72@gmail.com>
2026-03-14 16:11:21 +0100

    Initial Commit

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,22 @@
+import "react"
+import "./HomePage.css"
+
+export default function Home(){
+    return(
+        <>
+        <div className="HomeDashBoard">
+            Welcome Son
+            <img src="https://i.imgflip.com/8b9jjf.jpg"alt="penis" ></img>
+ 
+        </div>
+        </>
+    )
+}
+
+function Navbar(){
+    return(
+        <nav>
+
+        </nav>
+    )
+}
